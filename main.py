8a8cefb93d25bf44b395b14619f8561dab9c72a4
Trevor Mathisen <72931206+Trolann@users.noreply.github.com>
2021-08-23 17:09:39 +0000

    Initial build

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import 
+import discord
+import os
+
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN'] # Stored in secrets
+
+client = discord.Client()
+
+@client.event
+async def on_ready(): # When ready
+		print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message): # On every message
+		if message.author == client.user: # Ignore if it's a bot message
+			return
+
+		if message.content.startswith('$hello'):
+			await message.channel.send('Hello!')
+
+client.run(DISCORD_TOKEN)
