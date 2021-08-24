c8eae99b370c1ed49abb8600a0e053f92b9bb5de
Trevor Mathisen <72931206+Trolann@users.noreply.github.com>
2021-08-24 18:36:56 +0000

    on_member_join() is the devil

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import os
 from replit import db
 
+intents = discord.Intents.all()
+intents.members = True
 
 def count_message():
 	"""Counts total messages processed. Called on every on_message()"""
@@ -15,6 +17,11 @@ DISCORD_TOKEN = os.environ['DISCORD_TOKEN'] # Stored in secrets
 client = discord.Client()
 
 @client.event
+async def on_member_join(member):
+	print('Someone new!')
+	await member.send("Welcome")
+
+@client.event
 async def on_ready(): # When ready
 	print('We have logged in as {0.user}'.format(client))
 
@@ -25,7 +32,7 @@ async def on_message(message): # On every message
 	if message.author == client.user: # Cancel own message
 		return
 
-	if message.content.startswith('!'):
+	if message.content.startswith('?'):
 		await message.channel.send('Command')
 
 client.run(DISCORD_TOKEN)
